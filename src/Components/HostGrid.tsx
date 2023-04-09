0bac2289f42db29bc9ac54e745d3f2e0b963653a
agentroberto <sherwanirobert@gmail.com>
2023-04-08 23:08:04 -0500

    fix

--- src/Components/HostGrid.tsx
+++ src/Components/HostGrid.tsx
@@ -11,40 +11,38 @@ export const HostGrid = (data: any) => {
   return (
     <div>
       {data.data.map((e, i) => {
-        return (
-          <>
-            <Grid className={styles.hostgrid} key={i}>
-            <Link href={`/profile/${e.slug}`}> 
+        return <>
+          <Grid className={styles.hostgrid} key={i}>
+          <Link href={`/profile/${e.slug}`} legacyBehavior> 
 
-              <Grid xs={4}>
-                <Image
-                  className={styles.hostimage}
-                  src="/airbnb1.png"
-                  alt="Host Image"
-                  width={300}
-                  height={300}
-                  priority
-                />
-              </Grid>
-              </Link>
+            <Grid xs={4}>
+              <Image
+                className={styles.hostimage}
+                src="/airbnb1.png"
+                alt="Host Image"
+                width={300}
+                height={300}
+                priority
+              />
+            </Grid>
+            </Link>
 
-              <Grid xs={4}>
-                {e.location}
-                <Grid className={styles.ratingspan}>
-                  {e.rating} Stars
-                </Grid>
+            <Grid xs={4}>
+              {e.location}
+              <Grid className={styles.ratingspan}>
+                {e.rating} Stars
               </Grid>
-              <Grid xs={4}>
-                {e.host}
-              </Grid>
-              <Grid xs={4}>
-                ${e.price}
-              </Grid>
-          </Grid>
-        </>
-        )
+            </Grid>
+            <Grid xs={4}>
+              {e.host}
+            </Grid>
+            <Grid xs={4}>
+              ${e.price}
+            </Grid>
+        </Grid>
+      </>;
       })}
     </div>
-  )
+  );
 }
 
--- src/Components/NavBar/NavItem.tsx
+++ src/Components/NavBar/NavItem.tsx
@@ -3,11 +3,12 @@ import React from "react";
 
 const NavItem = ({ text, href, active }: any) => {
   return (
-    <Link href={href}
+    <Link
+      href={href}
       className={`nav__item ${
         active ? "active" : ""
       }`}
-    >
+      legacyBehavior>
       {text}
     </Link>
   );
